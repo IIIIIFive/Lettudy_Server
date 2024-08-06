6d2dea400376b0fb5d6d6a167fe7fe9a5aa0f40c
kkang0 <whye8510@hanyang.ac.kr>
2024-08-06 15:44:45 +0900

    [Feat/#14]: 채팅 API 구현

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const http = require("http");
 const cors = require("cors");
+const socketIo = require("socket.io");
 const { PORT } = require("./settings");
 
 const app = express();
@@ -7,6 +9,33 @@ const port = PORT || 7777;
 
 app.use(express.json());
 app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "DELETE"],
+  },
+});
+
+global.io = io;
+
+io.on("connection", (socket) => {
+  console.log("New Client connected");
+
+  socket.on("joinRoom", (roomId) => {
+    socket.join(roomId);
+    console.log(`Client joiend room: ${roomId}`);
+  });
+
+  socket.on("sendMessage", (message) => {
+    io.to(message.roomId).emit("receiveMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Lettudy Server");
@@ -21,10 +50,12 @@ const userRouter = require("./routes/usersRouter");
 const roomRouter = require("./routes/roomsRouter");
 const memberRouter = require("./routes/membersRouter");
 const scheduleRouter = require("./routes/shedulesRouter");
+const chatRouter = require("./routes/chatsRouter");
 
 app.use("/users", userRouter);
 app.use("/rooms", roomRouter);
 app.use("/members", memberRouter);
 app.use("/schedules", scheduleRouter);
+app.use("/chats", chatRouter);
 
 module.exports = app;

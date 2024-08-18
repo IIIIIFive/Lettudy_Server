145c9a837711851035da86f7a64039e3fb4a7510
Eunji <eunjipark0129@gmail.com>
2024-08-18 16:03:39 +0900

    serviceAcoount 파일 처리

--- settings.js
+++ settings.js
@@ -8,6 +8,19 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
+const FCM_TYPE = process.env.FCM_TYPE;
+const FCM_PROJECT_ID = process.env.FCM_PROJECT_ID;
+const FCM_PRIVATE_KEY_ID = process.env.FCM_PRIVATE_KEY_ID;
+const FCM_PRIVATE_KEY = process.env.FCM_PRIVATE_KEY;
+const FCM_CLIENT_EMAIL = process.env.FCM_CLIENT_EMAIL;
+const FCM_CLIENT_ID = process.env.FCM_CLIENT_ID;
+const FCM_AUTH_URI = process.env.FCM_AUTH_URI;
+const FCM_TOKEN_URI = process.env.FCM_TOKEN_URI;
+const FCM_AUTH_PROVIDER_X509_CERT_URL =
+  process.env.FCM_AUTH_PROVIDER_X509_CERT_URL;
+const FCM_CLIENT_X509_CERT_URL = process.env.FCM_CLIENT_X509_CERT_URL;
+const FCM_UNIVERSE_DOMAIN = process.env.FCM_UNIVERSE_DOMAIN;
+
 module.exports = {
   JWT_SECRET,
   PORT,
@@ -16,4 +29,15 @@ module.exports = {
   DB_USER,
   DB_PASSWORD,
   DB_NAME,
+  FCM_TYPE,
+  FCM_PROJECT_ID,
+  FCM_PRIVATE_KEY_ID,
+  FCM_PRIVATE_KEY,
+  FCM_CLIENT_EMAIL,
+  FCM_CLIENT_ID,
+  FCM_AUTH_URI,
+  FCM_TOKEN_URI,
+  FCM_AUTH_PROVIDER_X509_CERT_URL,
+  FCM_CLIENT_X509_CERT_URL,
+  FCM_UNIVERSE_DOMAIN,
 };

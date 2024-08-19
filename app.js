a352c1bb2297a8762ff3f5aa62a26416c478e7c0
kkang0 <whye8510@hanyang.ac.kr>
2024-08-19 17:38:34 +0900

    [Feat/#22]: 채팅방 통신 구현

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.get("/", (req, res) => {
   res.status(200).send("Lettudy Server");
 });
 
+app.get("/html", (req, res) => {
+  res.sendFile(join(__dirname, "/index.html"));
+});
+
 // 라우터 설정
 const userRouter = require("./routes/usersRouter");
 const roomRouter = require("./routes/roomsRouter");
@@ -34,6 +38,7 @@ const attendanceRouter = require("./routes/attendancesRouter");
 const noteRouter = require("./routes/notesRouter");
 const tagRouter = require("./routes/tagsRouter");
 const linkRouter = require("./routes/linksRouter");
+const { join } = require("path");
 
 app.use("/users", userRouter);
 app.use("/rooms", roomRouter);
--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,5 +1,10 @@
 const socketIo = require("socket.io");
 const chatService = require("../services/chatService");
+const chatsController = require("../controllers/chatsController");
+const CustomError = require("../utils/CustomError");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../settings");
+const conn = require("../utils/db");
 
 const initSocket = (server) => {
   const io = socketIo(server, {
@@ -13,38 +18,79 @@ const initSocket = (server) => {
 
   global.io = io;
 
+  io.use((socket, next) => {
+    const token =
+      socket.handshake.auth.token || socket.handshake.headers["authorization"];
+    if (!token) {
+      return next(new Error("Authentication error"));
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return next(new Error("Authentication error"));
+      }
+      socket.userId = decoded.id;
+      next();
+    });
+  });
+
   io.on("connection", (socket) => {
     console.log("New Client connected");
 
+    // socket.on("chat message", (message) => {
+    //   console.log("message: " + message);
+    //   io.emit("chat message", message);
+    // });
+
     socket.on("joinRoom", async (info) => {
-      const { roomId, userId } = info;
+      const { roomId } = info;
+      userId = socket.userId;
 
-      console.log(
-        `joinRoom event received with roomId: ${roomId}, userId: ${userId}`
-      );
-      try {
-        await chatService.checkMember(userId, roomId);
-        socket.join(roomId);
-        console.log(`Client joined room: ${roomId}`);
-      } catch (err) {
-        socket.emit("error", { message: err.message });
-      }
-    });
+      console.log("roomId: ", roomId);
+      console.log("userId: ", userId);
 
-    socket.on("sendMessage", async (message) => {
-      const { userId, roomId, content, type } = message;
-
-      console.log(`sendMessage: ${JSON.stringify(message)}`);
+      socket.join(roomId);
 
       try {
-        const chatData = await chatService.sendMessage(
-          userId,
-          roomId,
-          content,
-          type
+        const [userResult] = await conn.query(
+          `SELECT name FROM users WHERE id = ?`,
+          userId
         );
-        io.to(roomId).emit("receiveMessage", chatData);
+
+        if (userResult.length > 0) {
+          const userName = userResult[0].name;
+          console.log(`${userName} joined room: ${roomId}`);
+
+          io.to(roomId).emit("Join Room", { userId, userName });
+          // socket.to(roomId).emit(`joined Room`, { userId, userName });
+
+          // socket.removeAllListeners("chat message");
+
+          // 메시지 보내기
+          socket.on("chat message", async (message) => {
+            const { content, type } = message;
+
+            console.log("user id: ", userId);
+
+            try {
+              const result = await chatService.sendMessage(
+                userId,
+                roomId,
+                content,
+                type
+              );
+
+              io.to(roomId).emit("chat message", { userName, content });
+            } catch (err) {
+              console.log("Error saving message: ", err);
+              socket.emit("error", { message: err.message });
+            }
+          });
+        } else {
+          throw new CustomError("방이나 사용자를 찾을 수 없습니다.", 404);
+        }
       } catch (err) {
+        console.log("Error joining room: ", err);
         socket.emit("error", { message: err.message });
       }
     });

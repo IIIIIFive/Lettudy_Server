bea0117abab1591e05041cc6bb008d465b6d44d7
kkang0 <whye8510@hanyang.ac.kr>
2024-08-23 18:54:06 +0900

    [API/#50]: 채팅방 데이터 전달 수정

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -5,6 +5,9 @@ const CustomError = require("../utils/CustomError");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../settings");
 const conn = require("../utils/db");
+const memberQueries = require("../queries/memberQueries");
+const chatQueries = require("../queries/chatQueries");
+const userQueries = require("../queries/userQueries");
 
 const initSocket = (server) => {
   const io = socketIo(server, {
@@ -33,7 +36,40 @@ const initSocket = (server) => {
 
       try {
         await chatService.sendMessage(userId, roomId, content);
-        io.to(roomId).emit("chat", content);
+
+        const usernameResult = await conn.query(
+          userQueries.getNameById,
+          userId
+        );
+        const username = usernameResult[0][0].name;
+
+        const chatIdResult = await conn.query(
+          chatQueries.getChatIdByRoomId,
+          roomId
+        );
+        const chatId = chatIdResult[0][0]?.id;
+
+        const chatResult = await conn.query(chatQueries.getCreatedAt, [
+          chatId,
+          userId,
+        ]);
+        const createdAt = chatResult[0][0]?.created_at;
+
+        const profileNumResult = await conn.query(
+          memberQueries.getProfileNumByUserId,
+          [userId, roomId]
+        );
+        const profileNum = profileNumResult[0][0].profile_num;
+
+        const chatData = {
+          senderId: userId,
+          senderName: username,
+          content,
+          createdAt,
+          profileNum,
+        };
+
+        io.to(roomId).emit("chat", chatData);
       } catch (err) {
         console.log("Error saving message: ", err);
         socket.emit("error", { message: err.message });
@@ -44,72 +80,7 @@ const initSocket = (server) => {
       console.log("User disconnected");
       socket.disconnect();
     });
-
-    // 클라이언트 연결 종료 시
-    // socket.on("disconnect", (reason) => {
-    //   console.log(`User disconnected: ${socket.id}. Reason: ${reason}`);
-    //   const roomId = socket.handshake.query.roomId;
-    //   const userId = socket.userId;
-
-    //   if (roomId) {
-    //     socket.leave(roomId);
-    //     console.log(`User ${userId} left room ${roomId}`);
-    //   }
-    // });
   });
-
-  // io.on("connection", (socket) => {
-  //   socket.on("chat message", (msg) => {
-  //     io.emit("chat message", msg);
-  //   });
-  // });
-
-  // io.use((socket, next) => {
-  //   const token =
-  //     socket.handshake.auth.token || socket.handshake.headers["authorization"];
-  //   if (!token) {
-  //     return next(new Error("Authentication error"));
-  //   }
-
-  //   jwt.verify(token, JWT_SECRET, (err, decoded) => {
-  //     if (err) {
-  //       return next(new Error("Authentication error"));
-  //     }
-  //     socket.userId = decoded.id;
-  //     next();
-  //   });
-  // });
-
-  // io.on("connection", (socket) => {
-  //   const roomId = socket.handshake.query.roomId;
-  //   const userId = socket.userId;
-
-  //   if (!roomId) {
-  //     socket.disconnect();
-  //     return;
-  //   }
-
-  //   console.log(`New client connected to room: ${roomId}`);
-
-  //   socket.join(roomId);
-
-  //   socket.on("chat message", async (message) => {
-  //     const { content } = message;
-
-  //     try {
-  //       await chatService.sendMessage(userId, roomId, content);
-
-  //       io.to(roomId).emit("chat message", content);
-  //     } catch (err) {
-  //       console.log("Error saving message: ", err);
-  //       socket.emit("error", { message: err.message });
-  //     }
-  //   });
-
-  //   socket.on("disconnect", () => {
-  //     console.log(`Client disconnected from room: ${roomId}`);
-  //   });
-  // });
 };
 
 module.exports = initSocket;
